c5ed4b2c226e5188659ede9db2aefeaf358d28c2
sharpwell <161267796+sharpwell@users.noreply.github.com>
2024-02-29 15:17:42 +0800

    Create simplestreamquery.py

---
+++ example/streaming_query/simplestreamquery.py
@@ -0,0 +1,17 @@
+from proton_driver import client
+
+c = client.Client(host='127.0.0.1', port=8463, password=proton)
+
+# create a random stream if not exist
+c.execute("CREATE RANDOM STREAM IF NOT EXISTS"
+          " devices("
+          " device string default 'device'||to_string(rand()%4), "
+          " temperature float default rand()%1000/10"
+          ")")
+# query the stream and return in a iterator
+rows = c.execute_iter(
+    "SELECT device, count(*), min(temperature), max(temperature) "
+    "FROM devices GROUP BY device",
+)
+for row in rows:
+    print(row)
